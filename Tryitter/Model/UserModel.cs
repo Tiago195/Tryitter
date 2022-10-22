bb7f3abcd0e69235aced7b96a8bf3cc4f422c9e4
Tiago195 <tiagocf475@gmail.com>
2022-10-22 13:14:32 -0300

    fix: UserModel

--- Tryitter/Model/UserModel.cs
+++ Tryitter/Model/UserModel.cs
@@ -6,7 +6,9 @@ public class UserModel
 {
   [Key]
   public int UserId { get; set; }
+  [EmailAddress]
   public string Email { get; set; }
+  [MinLength(6)]
   public string Password { get; set; }
   public string Name { get; set; }
   public ICollection<PostModel>? Posts { get; set; }
